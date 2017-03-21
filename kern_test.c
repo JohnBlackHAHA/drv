c0dd47e1bbce2a136b85b64b4c9bf529f3b646fa
JohnBlackHAHA <787342510@qq.com>
2017-03-21 21:43:50 +0800

    Update kern_test.c

--- kern_test.c
+++ kern_test.c
@@ -49,25 +49,21 @@ int axi_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
     //if (temdata[read_count].length)
     //{
 	//printk(" %x\n",temdata[read_count].length);
-        if(copy_to_user((void *)buf,&temdata[read_count],sizeof(temdata[read_count])) != 0)
-			{
-				ERROR_LINE;
-				return -EFAULT;
-			}
-        temdata[read_count]=tmp;
-	temdata[read_count].length=0;
-	temdata[read_count].type=0;
+    if(copy_to_user((void *)buf,&temdata[read_count],sizeof(temdata[read_count])) != 0)
+    {
+	ERROR_LINE;
+        return -EFAULT;
+    }
+    temdata[read_count]=tmp;
+    temdata[read_count].length=0;
+    temdata[read_count].type=0;
    // }
     if (read_count !=199 )
         read_count++;
     else
         read_count = 0;
     //}
-
-    return 0;
-
-
-
+    return 0;	
 }
 
 
@@ -79,8 +75,7 @@ int axi_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
 **************************/
 int axi_write(struct file *filp,const char __user *buf, size_t size, loff_t *ppos)
 {
-
-	return 0;
+    return 0;
 }
 
 
@@ -93,8 +88,7 @@ int axi_write(struct file *filp,const char __user *buf, size_t size, loff_t *ppo
 **************************/
  int  axi_mmap(struct file *filp, struct vm_area_struct *vm_areap)
 {
-
-	return 0;
+    return 0;
 }
 
 
@@ -108,15 +102,15 @@ int axi_write(struct file *filp,const char __user *buf, size_t size, loff_t *ppo
  **************************/
  int axi_open(struct inode * inode,struct file *filp)
 {
-	return 0;
+    return 0;
 }
 
  void ShowOpInfo(void *pOp)
  {
-	TAxiRwReg *pReg = (TAxiRwReg *)pOp;
-	printk("pReg->nRead    : 0x%x\n",pReg->nRead);
-	printk("pReg->nRegAddr : 0x%x\n",pReg->nRegAddr);
-	printk("pReg->nRegVal  : 0x%x\n",pReg->nRegVal);
+    TAxiRwReg *pReg = (TAxiRwReg *)pOp;
+    printk("pReg->nRead    : 0x%x\n",pReg->nRead);
+    printk("pReg->nRegAddr : 0x%x\n",pReg->nRegAddr);
+    printk("pReg->nRegVal  : 0x%x\n",pReg->nRegVal);
  }
 
 
@@ -129,39 +123,36 @@ int axi_write(struct file *filp,const char __user *buf, size_t size, loff_t *ppo
 
 static long axi_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	TAxiRwReg nRegOp = {0};
-	int ret = 0;
-
-	//printk("Get IOCTL Cmd %d with args %lu\n",cmd,arg);
-	switch(cmd)
-	{
-			case AXI_CMD_RW_REG:
-			if(copy_from_user(&nRegOp,(void *)arg,sizeof(nRegOp))!= 0)
-			{
-				ERROR_LINE;
-				return -EFAULT;
-			}
-			//ShowOpInfo(&nRegOp);
-			if(nRegOp.nRead == 1)
-			{
-				nRegOp.nRegVal  = *((const volatile u32 *) (gVirtMem + nRegOp.nRegAddr));
-			}else{
-				*(volatile u32  *) (gVirtMem + nRegOp.nRegAddr) = nRegOp.nRegVal;
-			}
-			if(copy_to_user((void *)arg,&nRegOp,sizeof(nRegOp)) != 0)
-			{
-				ERROR_LINE;
-				return -EFAULT;
-			}
-			break;
-
-		default:
-			ERROR_LINE;
-			ret = -EFAULT;
-			break;
+    TAxiRwReg nRegOp = {0};
+    int ret = 0;
+    //printk("Get IOCTL Cmd %d with args %lu\n",cmd,arg);
+    switch(cmd)
+    {
+        case AXI_CMD_RW_REG:
+        if(copy_from_user(&nRegOp,(void *)arg,sizeof(nRegOp))!= 0)
+        {
+           ERROR_LINE;
+           return -EFAULT;
+        }
+    //ShowOpInfo(&nRegOp);
+        if(nRegOp.nRead == 1)
+        {
+            nRegOp.nRegVal  = *((const volatile u32 *) (gVirtMem + nRegOp.nRegAddr));
+	}else{
+                 *(volatile u32  *) (gVirtMem + nRegOp.nRegAddr) = nRegOp.nRegVal;
+	     }
+        if(copy_to_user((void *)arg,&nRegOp,sizeof(nRegOp)) != 0)
+        {
+            ERROR_LINE;
+	    return -EFAULT;
 	}
-
-	return ret;
+	break;
+	default:
+	ERROR_LINE;
+	ret = -EFAULT;
+        break;
+    }
+    return ret;
 }
 
 
@@ -169,7 +160,7 @@ static long axi_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 #ifdef CONFIG_COMPAT
 static long axi_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	return axi_ioctl(filp, cmd, (unsigned long)compat_ptr(arg));
+    return axi_ioctl(filp, cmd, (unsigned long)compat_ptr(arg));
 }
 #else
 #define axi_compat_ioctl NULL
@@ -183,20 +174,19 @@ static long axi_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long 
 **************************/
  int axi_release(struct inode * inode,struct file *filp)
 {
-
-	return 0;
+    return 0;
 }
 
 const struct file_operations axi_drv_fops=
 {
-	.owner = THIS_MODULE,
-	.read = axi_read,
-	.write = axi_write,
-	.unlocked_ioctl = axi_ioctl,
-	.compat_ioctl = axi_compat_ioctl,
-	.open = axi_open,
-	.release = axi_release,
-	.mmap = axi_mmap,
+    .owner = THIS_MODULE,
+    .read = axi_read,
+    .write = axi_write,
+    .unlocked_ioctl = axi_ioctl,
+    .compat_ioctl = axi_compat_ioctl,
+    .open = axi_open,
+    .release = axi_release,
+    .mmap = axi_mmap,
 };
 /*************************
 *function		:
@@ -268,54 +258,54 @@ irqreturn_t axi_irq_handle1(int nIrq,void *pArgs)
 **************************/
 int axi_drv_init(void)
 {
-	int status = 0;
-	int minor = -1;
-	int ret[3] = {0};//中断请求返回值数组
-	tasklet_init(&my_tasklet,fpga_data,0);
-	printk("Driver Release %s %s\n",__DATE__,__TIME__);
-	status = register_chrdev(AXIDEV_MAJOR, "axi", &axi_drv_fops);//主设备号，设备名，驱动的文件方法
-	if (status < 0)
-		return status;
-	gVirtMem = ioremap(0x40000000,0x20000);
-	if (IS_ERR(gVirtMem))
-	{
-		ERROR_LINE;
-		return -1;
-	}
-	//*(volatile u32  *) (gVirtMem + 0x100c) = 1;
-	gDevClass = class_create(THIS_MODULE, "axi_dev");//第一个参数指代当前模块
-	if (IS_ERR(gDevClass))
-	{
-		unregister_chrdev(AXIDEV_MAJOR, "axi");
-		ERROR_LINE;
-		return PTR_ERR(gDevClass);
-	}
-	/*if(gpio_request(61,"AXI_IRQ") < 0)
-	{
-		printk(KERN_INFO "Request GPIO Failed.\n");
-	}else{
-		if(gpio_direction_input(61) < 0)
-		{
-			printk(KERN_INFO "Set GPIO Dir Failed.\n");
-		}else{
+    int status = 0;
+    int minor = -1;
+    int ret[3] = {0};//中断请求返回值数组
+    tasklet_init(&my_tasklet,fpga_data,0);
+    printk("Driver Release %s %s\n",__DATE__,__TIME__);
+    status = register_chrdev(AXIDEV_MAJOR, "axi", &axi_drv_fops);//主设备号，设备名，驱动的文件方法
+    if (status < 0)
+        return status;
+    gVirtMem = ioremap(0x40000000,0x20000);
+    if (IS_ERR(gVirtMem))
+    {
+        ERROR_LINE;
+        return -1;
+    }
+    //*(volatile u32  *) (gVirtMem + 0x100c) = 1;
+    gDevClass = class_create(THIS_MODULE, "axi_dev");//第一个参数指代当前模块
+    if (IS_ERR(gDevClass))
+    {
+        unregister_chrdev(AXIDEV_MAJOR, "axi");
+        ERROR_LINE;
+        return PTR_ERR(gDevClass);
+    }
+  /*if(gpio_request(61,"AXI_IRQ") < 0)
+    {
+        printk(KERN_INFO "Request GPIO Failed.\n");
+    }else{
+             if(gpio_direction_input(61) < 0)
+             {
+                 printk(KERN_INFO "Set GPIO Dir Failed.\n");
+	     }else{
 		    irq[0]=gpio_to_irq(61);*/
-			if((ret[0] = request_irq(61, axi_irq_handle1, IRQF_TRIGGER_RISING ,"axi_dev",NULL)) < 0)
-			{
-				printk(KERN_INFO "Request IRQ Failed with %d\n",ret[0]);
-			}
-		//}
+		    if((ret[0] = request_irq(61, axi_irq_handle1, IRQF_TRIGGER_RISING ,"axi_dev",NULL)) < 0)
+		    {
+			printk(KERN_INFO "Request IRQ Failed with %d\n",ret[0]);
+		    }
 	//}
-	minor = find_first_zero_bit(minors, N_AXI_MINORS);
-	if (minor < N_AXI_MINORS) {
-		struct device *dev;
-		gDevt = MKDEV(AXIDEV_MAJOR, minor);
-		dev = device_create(gDevClass, NULL, gDevt,NULL, "axi_dev");
-		status = IS_ERR(dev) ? PTR_ERR(dev) : 0;
+    //}
+    minor = find_first_zero_bit(minors, N_AXI_MINORS);
+    if (minor < N_AXI_MINORS) {
+	   struct device *dev;
+	   gDevt = MKDEV(AXIDEV_MAJOR, minor);
+	   dev = device_create(gDevClass, NULL, gDevt,NULL, "axi_dev");
+	   status = IS_ERR(dev) ? PTR_ERR(dev) : 0;
 	} else {
-		status = -ENODEV;
-	}
-	if (status == 0) {
-		set_bit(minor, minors);
+		   status = -ENODEV;
+               }
+    if (status == 0) {
+        set_bit(minor, minors);
 	}
 	return 0;
 }
@@ -329,14 +319,14 @@ int axi_drv_init(void)
 **************************/
 void axi_drv_exit(void)
 {
-	*(volatile u32  *) (gVirtMem + 0x100c) = 0;
-	iounmap(gVirtMem);
-	free_irq(61,NULL);
-	//gpio_free(61);
-	//printk (KERN_INFO "%d",count);
-	device_destroy(gDevClass, gDevt);
-	class_destroy(gDevClass);
-	unregister_chrdev(AXIDEV_MAJOR, "axi_driver");
+    *(volatile u32  *) (gVirtMem + 0x100c) = 0;
+    iounmap(gVirtMem);
+    free_irq(61,NULL);
+    //gpio_free(61);
+    //printk (KERN_INFO "%d",count);
+    device_destroy(gDevClass, gDevt);
+    class_destroy(gDevClass);
+    unregister_chrdev(AXIDEV_MAJOR, "axi_driver");
 }
 MODULE_AUTHOR("John Black");
 MODULE_LICENSE("Dual BSD/GPL");
